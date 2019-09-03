ccd43826b70b0e1df50c04dcb4c3ef25f00e03e1
junyeopp <junyeop@sogang.ac.kr>
2019-09-03 23:00:11 +0900

    class22

--- main.c
+++ main.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void){
-    printf("TEST \n 1\bTEST2");
-
-    system("pause");
-    return 0;
+int add(int a, int b) { return a + b; }
+int (*process(char *a))(int, int) {
+  printf("%s\n", a);
+  return add;
+}
+int main(void) {
+  printf("%d\n", process("10 + 20")(10, 20));
+  return 0;
 }
